1f6fcdd84ca2a42dd89f92c16101743c435a3618
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-07-29 20:51:04 -0300

    feat: change settings for PythonAnywhere deploy

--- dtc/dtc/settings.py
+++ dtc/dtc/settings.py
@@ -25,12 +25,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = (
+    "++a!s1(24--n07k21f9bb8a&w3=0xc(absv=3g*pt90q%e51%^"  # os.getenv("SECRET_KEY")
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "1"
+DEBUG = 1  # os.getenv("DEBUG") == "1"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "[::1]",
+    "higorrsc.pythonanywhere.com",
+]  # os.getenv("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
